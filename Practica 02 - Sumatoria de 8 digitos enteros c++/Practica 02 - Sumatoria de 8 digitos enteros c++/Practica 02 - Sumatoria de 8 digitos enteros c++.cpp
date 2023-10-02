12a24e1369fcdc08f8604270f453e48a1dc9e7cc
Horacio09 <hmorenoe74@gmail.com>
2023-10-02 17:37:57 -0600

    Sumatoria de 8 digitos enteros c++

---
+++ Practica 02 - Sumatoria de 8 digitos enteros c++/Practica 02 - Sumatoria de 8 digitos enteros c++/Practica 02 - Sumatoria de 8 digitos enteros c++.cpp
@@ -0,0 +1,19 @@
+//Horacio Moreno Hernández 3-J LCS python
+
+#include <iostream>
+using namespace std;
+const int num = 8;
+
+int main()
+{
+    int nums[num];
+    int total = 0;
+    for (int i = 0; i < num; i++)
+    {
+        cout << "Por favor, Introduza el numero: ";
+        cin >> nums[i];
+        total += nums[i];
+    }
+    cout << "El total de numero es: " << total << endl;
+    return 0;
+}
