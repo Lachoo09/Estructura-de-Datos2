772e44dbd176a3f4d6013b4f19d6d7557d288b13
Horacio09 <hmorenoe74@gmail.com>
2023-10-02 17:29:15 -0600

    Sumatoria de 8 digitos enteros

--- Practica 02 - Sumatoria de 8 digitos enteros/Practica 02 - Sumatoria de 8 digitos enteros/Practica_02___Sumatoria_de_8_digitos_enteros.py
+++ Practica 02 - Sumatoria de 8 digitos enteros/Practica 02 - Sumatoria de 8 digitos enteros/Practica_02___Sumatoria_de_8_digitos_enteros.py
@@ -1,1 +1,9 @@
+#Horacio Moreno Hernández 3-J LCS python
 
+NUM = 8
+nums = [0] * NUM 
+total = 0
+for i in range(NUM):
+        nums[i] = int(input("Por favor, introduzca el numero: "))
+        total += nums[i]
+        print("El total de numeros es: ", total)
