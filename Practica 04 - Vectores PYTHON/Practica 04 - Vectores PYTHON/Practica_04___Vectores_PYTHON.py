5c16df499169944f386bdaa57dd54d0307dc11db
Horacio09 <142040799+Lachoo09@users.noreply.github.com>
2023-10-06 14:00:58 -0600

    Practica 04 - Vectores

---
+++ Practica 04 - Vectores PYTHON/Practica 04 - Vectores PYTHON/Practica_04___Vectores_PYTHON.py
@@ -0,0 +1,19 @@
+#Horacio Moreno Hernandez 3-J LSC
+
+Aves = ["Loro gris", "Paloma de diamanete", "Coctel"]
+print("Los valores de la lista antes de insertar: ")
+
+for ave in Aves:
+    print(ave, end = " ")
+    
+print("\n")
+
+Aves.append("Mayna")
+Aves.append("Periquitos")
+Aves.append("Cacatua")
+print("Los valores de la lista despues de insertar: ")
+
+for ave in Aves:
+    print(ave,end = " ")
+    
+print("\n")
